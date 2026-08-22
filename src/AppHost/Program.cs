9bb44fef18dc03cdfdd9ea7c7d75d9ed708648bc
gigasmash476996-pixel <nguyenngochoang.dev@gmail.com>
2026-08-22 23:37:36 +0700

    Thiet lap ha tang CSDL va xoa scratch

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -1,3 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var redis = builder.AddRedis("redis-cache");
+var postgres = builder.AddPostgres("postgres-db").WithPgAdmin();
+var rabbitmq = builder.AddRabbitMQ("rabbitmq-bus").WithManagementPlugin();
+
 builder.Build().Run();
